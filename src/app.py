9b4006f4ec40cf6a3cc5180626bd0321f67998b3
Ezequiel SM <seguramacedoezequiel@gmail.com>
2022-07-01 21:52:11 -0500

    master

---
+++ src/app.py
@@ -0,0 +1,79 @@
+from flask import Flask, request, jsonify
+from flask_sqlalchemy import SQLAlchemy
+from flask_marshmallow import Marshmallow
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root@localhost/biblioteca'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+ma = Marshmallow(app)
+
+
+class Libros(db.Model):
+    isbn = db.Column(db.Integer, primary_key=True)
+    titulo = db.Column(db.String(100), unique=True)
+    autor = db.Column(db.String(100))
+
+    def __init__(self, isbn, titulo, autor):
+        self.isbn = isbn
+        self.titulo = titulo
+        self.autor = autor
+
+db.create_all()
+
+class LibroSchema(ma.Schema):
+    class Meta:
+        fields = ('isbn', 'titulo', 'autor')
+
+
+libro_schema = LibroSchema()
+libros_schema = LibroSchema(many=True)
+
+@app.route('/libros', methods=['Post'])
+def create_libro():
+    isbn = request.json['isbn']
+    titulo = request.json['titulo']
+    autor = request.json['autor']
+    new_libro = Libros(isbn, titulo, autor)
+    db.session.add(new_libro)
+    db.session.commit()
+    return libro_schema.jsonify(new_libro)
+
+@app.route('/libros/', methods=['GET'])
+def get_libros():
+    todos_libros = Libros.query.all()
+    result = libros_schema.dump(todos_libros)
+    return jsonify(result)
+
+@app.route('/libros/<isbn>', methods=['GET'])
+def get_libro(isbn):
+    libro = Libros.query.get(isbn)
+    return libro_schema.jsonify(libro)
+
+@app.route('/libros/<isbn>', methods=['PUT'])
+def update_libro(isbn):
+    libro = Libros.query.get(isbn)
+    isbn = request.json['isbn']
+    titulo = request.json['titulo']
+    autor = request.json['autor']
+    libro.isbn = isbn
+    libro.titulo = titulo
+    libro.autor = autor
+    db.session.commit()
+    return libro_schema.jsonify(libro)
+
+@app.route('/libros/<isbn>', methods=['DELETE'])
+def delete_libro(isbn):
+    libro = Libros.query.get(isbn)
+    db.session.delete(libro)
+    db.session.commit()
+    return libro_schema.jsonify(libro)
+
+
+@app.route('/', methods=['GET'])
+def index():
+    return jsonify({'message': 'Welcome to my API'})
+
+if __name__ == "__main__":
+    app.run(debug=True)
